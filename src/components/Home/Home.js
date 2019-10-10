6858c7d1bf9da2f3792ac60b8666ebc643a8c093
KaetiG <kaetigust@gmail.com>
2019-10-10 12:44:15 -0500

    added semantic ui grid

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
+// import Semantic UI Components 
+import { Button, Icon, Header, Grid } from 'semantic-ui-react'
+
 class Home extends Component {
     render(){
         return (
             <>
+            <Grid>
+                <Grid.Column width={6}>
+                    <>
+                    </>
+                </Grid.Column>
+                <Grid.Column width={4}>
+                    Drop downs here
+                </Grid.Column>
+                <Grid.Column width={6}>
+                    <>
+                    </>
+                </Grid.Column>
+            </Grid>
             </>
         )
     }
